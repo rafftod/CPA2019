a10b2a31b29e6f96d821f3021f5839649aaaab78
raniacharkaoui <48803445+raniacharkaoui@users.noreply.github.com>
2019-11-15 17:56:32 +0100

    Add files via upload

---
+++ test_phr.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	ifstream in ("P00533.fasta.phr", ios::binary);
+	ofstream out ("out_phr.txt");
+	
+	if( in.is_open() )
+	{
+		uint8_t x;
+		
+		while( in.read((char *) (&x), sizeof(x) )){ //lecture byte par byte (sizeof(x) = 1 )
+			cout << hex << (int32_t) x << endl;
+		}
+	}
+	else
+		cout << "Impossible d'ouvrir le fichier" << endl;
+		
+	in.close();
+	out.close();
+	cout << endl;
+	
+	return 0;
+	
+}
