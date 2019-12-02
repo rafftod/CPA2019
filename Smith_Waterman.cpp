91ffb71153a1447e9249a6d9d0dcc32e821e5e67
Vlad696969 <vstefan1399@gmail.com>
2019-12-02 21:06:07 +0100

    Smith Waterman scoring matrix

--- Smith_Waterman.cpp
+++ Smith_Waterman.cpp
@@ -11,31 +11,93 @@ Smith_Waterman::Smith_Waterman(int gap1, int gap2, int m, int nm){
 
 Smith_Waterman::~Smith_Waterman(){}
 
-void Smith_Waterman::compare(std::string& sequence1, std::string& sequence2){
+void Smith_Waterman::compare(uint8_t* sequence1, std::vector<int>& sequence2, int length1, int length2)
+{
     //compare 2 sequences with the Smith-Waterman algorithm
+    //sequence1 : sequence from the database
+    //sequence2 : query sequence
+    //length1 : number of residues in sequence1 + 1
+    //length2 : number of residues in sequence2 + 1
 
     //initialize score matrix
     
-    matrix =  new int*[sequence1.length()];//sequence1.length() = number of rows
-    for (int i = 0; i < sequence1.length(); ++i){
-        matrix[i] = new int[sequence2.length()];//sequence.length() = number of columns
+    matrix =  new int*[length1];//length1 = number of rows
+    for (int i = 0; i < length1; ++i)
+    {
+        matrix[i] = new int[length2];//length2 = number of columns
     }
 
     //first row and column are set at 0
 
-    for (int i = 0; i < sequence1.length(); i++){
+    for (int i = 0; i < length1; i++)
+    {
         matrix[i][0]=0;
     }
 
-    for (int j = 1; j < sequence2.length(); j++){
+    for (int j = 1; j < length2; j++)
+    {
         matrix[0][j]=0;
     }
 
-    //compare each residu and change score accordingly
+    //scoring matrix
 
-    for(int i = 0; i < sequence1.length(); i++){
-        for (int j = 0; j < sequence2.length(); j++){
-
+    for(int i = 1; i < length1; i++)
+    {
+        for (int j = 1; j < length2; j++)
+        {
+            int a = matrix[i-1][j-1] + blosum_matrix[sequence1[i]][sequence2[j]];
+            int b = max_column(i,j,matrix);
+            int c = max_row(i,j,matrix);
+            matrix[i][j] = find_max(a,b,c);
         }
     }
+}
+
+int Smith_Waterman::find_max(int a, int b, int c)
+{
+    int max = 0;
+    if (a > max)
+    {
+        max = a;
+    }
+
+    if (b > max)
+    {
+        max = b;
+    }
+    if (c > max)
+    {
+        max = c;
+    }
+    return max;
+}
+
+int Smith_Waterman::max_column(int i, int j, int** matrix)
+//returns the maximum score on a column
+{
+    int max = matrix[i][j-1] - gap_penalty_exp - gap_penalty_open;
+    for (int k = 2; k <= i; k++)
+    {
+        int score = matrix[i][j - k] - k*gap_penalty_exp - gap_penalty_open;
+        if (score > max)
+        {
+            max = score;
+        }  
+    }
+    return max;
+}
+
+int Smith_Waterman::max_row(int i, int j, int** matrix)
+//returns the maximum score on a line
+{
+    int max = matrix[i-1][j] - gap_penalty_exp - gap_penalty_open;
+    for (int k = 2; k <= i; k++)
+    {
+        int score = matrix[i - k][j] - k*gap_penalty_exp - gap_penalty_open;
+        if (score > max)
+        {
+            max = score;
+        }  
+    }
+    return max;
 }
--- Smith_Waterman.h
+++ Smith_Waterman.h
@@ -2,21 +2,25 @@
 #define SMITH_WATERMAN
 #include <iostream>
 #include <string>
+#include <vector>
 
 class Smith_Waterman{
 private:
     int** matrix;
-    int gap_penalty_open;
-    int gap_penalty_exp;
+    int gap_penalty_open = 11;
+    int gap_penalty_exp = 1;
     int match;
     int no_match;
-    int blosom_matrix[24][24]; // Al BLOSUM matrix are 24 x 24
+    int blosum_matrix[24][24]; // Al BLOSUM matrix are 24 x 24
 
 public:
     Smith_Waterman(int gap1,int gap2,int m, int nm);
     ~Smith_Waterman();
 
-    void compare(std::string& sequence1,std::string& sequence2);
+    void compare(uint8_t* sequence1,std::vector<int>& sequence2, int length1, int length2);
+    int find_max(int a, int b, int c);
+    int max_column(int i, int j, int** matrix);
+    int max_row(int i, int j, int** matrix);
 
 };
 #endif
